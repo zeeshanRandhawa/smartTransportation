ca7dbaedf5701922eea850270ea60d44dd98e894
zeeshanRandhawa <zeeshan.randhawa@questlab.pk>
2023-04-14 02:52:23 +0500

    added demo

--- app.js
+++ app.js
@@ -189,9 +189,9 @@ app.post("/api/datetime", getNodesBasedOnDateTimePost)
 app.get("/demo", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "demo.html"));
   });
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "public", "index.html"));
-//   });
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
   
 
   
